3fc542a914dd582d7b6347bf8084ca396784ab5d
Lin Wei-Chih <34365220+kjoelovelife@users.noreply.github.com>
2019-05-16 09:55:27 +0800

    Update jetson_nano_usefultools.sh

--- jetson_nano_usefultools.sh
+++ jetson_nano_usefultools.sh
@@ -38,10 +38,18 @@ sudo apt-get upgrade
 
 #=========================  Install ROS melodic =======================================
 cd
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-sudo apt-get update
 
+# enable all Ubuntu packages:
+sudo apt-add-repository universe
+sudo apt-add-repository multiverse
+sudo apt-add-repository restricted
+
+# add ROS repository to apt sources
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+
+# install ROS desktop-full
+sudo apt-get update
 sudo apt install -y ros-melodic-desktop-full
 
 sudo rosdep init
