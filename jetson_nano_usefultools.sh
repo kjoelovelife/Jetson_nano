f36c0b21e78335c2bd7e91488d4e8fa0a22ce250
Lin Wei-Chih <34365220+kjoelovelife@users.noreply.github.com>
2019-06-05 18:13:14 +0800

    Update jetson_nano_usefultools.sh

--- jetson_nano_usefultools.sh
+++ jetson_nano_usefultools.sh
@@ -208,8 +208,8 @@ sudo ldconfig
 
 #==== Install Darknet , let jetson-nano can train Yolo model with darknet. ====
 #echo "export CUBA_HOME=/usr/local/cuda-10.0"
-#echo "export PATH=/usr/local/cuda-10.0/bin:$PATH" >> ~/.bashrc
-#echo "export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
+#echo "export PATH=${PATH}:/usr/local/cuda/bin" >> ~/.bashrc
+#echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda/lib64" >> ~/.bashrc
 #source ~/.bashrc
 #cd ~/Jetson_nano/darknet
 #sudo make -j4
