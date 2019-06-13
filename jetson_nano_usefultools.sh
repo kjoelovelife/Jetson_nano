2e6de960358609ab5b7c8ebdaaf71b279b05ab03
Lin Wei-Chih <34365220+kjoelovelife@users.noreply.github.com>
2019-06-13 14:51:07 +0800

    Update jetson_nano_usefultools.sh

--- jetson_nano_usefultools.sh
+++ jetson_nano_usefultools.sh
@@ -130,8 +130,7 @@ pip3 install matplotlib \
 		 scipy
 # also can install :pillow
 
-sudo pip3 install --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu==1.13.1+nv19.4 \
-                  jupyter notebook
+sudo pip3 install --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu==1.13.1+nv19.4
 
 sudo pip3 install --upgrade numpy
 
