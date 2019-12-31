2852e7636fa00da101109e83cf163cde8ab0089b
Joe Lin <kjoelovelife>
2019-12-31 16:45:15 +0800

    midified jetbot_ros_dependiences.sh

--- jetbot_ros/set_ros_master.sh
+++ jetbot_ros/set_ros_master.sh
@@ -8,7 +8,7 @@ else
 	export ROS_MASTER_URI=http://$HOSTNAME.local:11311/
 fi
 
-export ROS_HOSTNAME=http://$HOSTNAME.local:11311/
+export ROS_HOSTNAME=$HOSTNAME.local
 
 echo "ROS_MASTER_URI set to $ROS_MASTER_URI"
 echo "ROS_HOSTNAME set to $HOSTNAME.local"
