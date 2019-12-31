222841b5648c67e5aca209e5881d63caa9d1364c
Joe Lin <kjoelovelife>
2019-12-31 16:32:42 +0800

    set_ros_master.sh

---
+++ jetbot_ros/set_ros_master.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+echo "Setting ROS_MASTER_URI..."
+if [ $# -gt 0 ]; then
+	# provided a hostname, use it as ROS_MASTER_URI
+	export ROS_MASTER_URI=http://$1.local:11311/
+else
+	echo "No hostname provided. Using $HOSTNAME."
+	export ROS_MASTER_URI=http://$HOSTNAME.local:11311/
+fi
+
+export ROS_HOSTNAME=http://$HOSTNAME.local:11311/
+
+echo "ROS_MASTER_URI set to $ROS_MASTER_URI"
+echo "ROS_HOSTNAME set to $HOSTNAME.local"
