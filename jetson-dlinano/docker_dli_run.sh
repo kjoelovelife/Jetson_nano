fb7b76f43b88a3717a6ffec4bc98b05645edaae9
Joe Lin <kjoelovelife@gmail.com>
2020-10-30 13:05:25 +0800

    modified jetson-dlinano/docker_dli_run.sh

--- jetson-dlinano/docker_dli_run.sh
+++ jetson-dlinano/docker_dli_run.sh
@@ -1,5 +1,55 @@
-sudo docker run --runtime nvidia -it --rm --network host \
---volume ~/nvdli-data:/nvdli-nano/data \
---volume /tmp/argus_socket:/tmp/argus_socket \
---device /dev/video0 \
-nvcr.io/nvidia/dli/dli-nano-ai:v2.0.1-r32.4.4
+
+#!/usr/bin/env bash
+
+# Shell script scripts to install useful tools , such as opencv , pytorch...
+# -------------------------------------------------------------------------
+#Copyright © 2020 Wei-Chih Lin , kjoelovelife@gmail.com 
+#update : 2020.10.26
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+# -------------------------------------------------------------------------
+# reference
+# https://chtseng.wordpress.com/2019/05/01/nvida-jetson-nano-%E5%88%9D%E9%AB%94%E9%A9%97%EF%BC%9A%E5%AE%89%E8%A3%9D%E8%88%87%E6%B8%AC%E8%A9%A6/
+#
+# -------------------------------------------------------------------------
+
+echo "Welcome to class Getting Started with AI on Jetson Nano !"
+echo "Please choose your camera.If you don't type anything , default is usb camera."
+echo "(U/u : usb camera  ,  C/c : CSI camera.)"
+echo -n "Your camera is : "
+read camera
+
+if [ "$camera" '==' "C" ] || [ "$camera" '==' "c" ];
+then
+    sudo docker run --runtime nvidia -it --rm --network host \
+    --volume ~/nvdli-data:/nvdli-nano/data \
+    --volume /tmp/argus_socket:/tmp/argus_socket \
+    --device /dev/video0 \
+    nvcr.io/nvidia/dli/dli-nano-ai:v2.0.1-r32.4.4
+else
+    sudo docker run --runtime nvidia -it --rm --network host \
+    --volume ~/nvdli-data:/nvdli-nano/data \
+    --device /dev/video0 \
+    nvcr.io/nvidia/dli/dli-nano-ai:v2.0.1-r32.4.4
+fi
+
+
+
+
